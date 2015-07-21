a2d15fb91d6e6878440e479f1b6cb098e8a4605a
Vincent Zhang <vzhang@purdue.edu>
2015-07-21 16:14:31 -0400

    Added an exception for invalid configs

---
+++ src/main/java/co/phoenixlab/ttvradio/config/InvalidConfigException.java
@@ -0,0 +1,21 @@
+package co.phoenixlab.ttvradio.config;
+
+import java.io.IOException;
+
+public class InvalidConfigException extends IOException {
+
+    public InvalidConfigException() {
+    }
+
+    public InvalidConfigException(String message) {
+        super(message);
+    }
+
+    public InvalidConfigException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidConfigException(Throwable cause) {
+        super(cause);
+    }
+}
